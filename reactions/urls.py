807ae7693e477e47a845c8f1db20cb0d78075fb6
spacenot4me <melentev022@gmail.com>
2024-05-08 12:58:52 +0500

    posts changes, liked posts function

--- reactions/urls.py
+++ reactions/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('commnets/', AllComments.as_view()),
     path('likes/<int:pk>', LikesView.as_view()),
     path('commnets/<int:pk>', CommentsView.as_view()),
-    path('reactions/count/<int:post_id>', get_reactions_count, name='get_likes_by_post'),
-    path('reactions/check/like/<int:post_id>/<int:owner_id>', is_liked, name='is_post_liked_by_user'),
+    path('reactions/count/<int:post_id>/<int:owner_id>', get_reactions_count, name='get_likes_by_post_and_is_user_liked_post'),
     path('reactions/likedposts/<int:owner_id>', get_liked_posts, name='get_liked_posts_of_user')
 ]
--- reactions/views.py
+++ reactions/views.py
@@ -5,6 +5,7 @@ from reactions.models import Like, Comment
 from reactions.serializers import LikesSerializer, CommentsSerializer
 import requests
 
+
 # Create your views here.
 
 
@@ -32,7 +33,7 @@ class LikesView(generics.RetrieveDestroyAPIView):
     lookup_field = 'pk'
 
 
-def get_reactions_count(request, post_id):
+def get_reactions_count(request, post_id, owner_id):
     """
     View-функция для подсчета лайков по post_id.
 
@@ -46,24 +47,30 @@ def get_reactions_count(request, post_id):
 
     comment_count = Comment.objects.filter(post_id=post_id).count()
     like_count = Like.objects.filter(post_id=post_id).count()
-    data = {
-        "comment_count": comment_count,
-        "like_count": like_count
-    }
-    return JsonResponse(data)
-
-
-def is_liked(request, owner_id, post_id):
     if Like.objects.filter(owner_id=owner_id, post_id=post_id).exists():
-        return JsonResponse({"is_liked": True})
+        data = {
+            "comment_count": comment_count,
+            "like_count": like_count,
+            "is_liked": True
+        }
+
+        return JsonResponse(data)
     else:
-        return JsonResponse({"is_liked": False})
+        data = {
+            "comment_count": comment_count,
+            "like_count": like_count,
+            "is_liked": False
+        }
+        return JsonResponse(data)
+
 
 """
 def get_liked_posts(request, owner_id):
     liked_posts = Like.objects.filter(owner_id=owner_id).values_list('post_id', flat=True)
     return JsonResponse({"liked_posts": list(liked_posts)})
 """
+
+
 def get_liked_posts(request, owner_id):
     liked_posts_ids = Like.objects.filter(owner_id=owner_id).values_list('post_id', flat=True)
     liked_posts = []
